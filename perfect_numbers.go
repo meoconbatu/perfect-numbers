19c027a00143b737a80becfc896c0d3b96e01fd0
Thanh Mi Nguyen Thanh <thanhmi1111@live.com>
2017-04-18 21:52:54 +0700

    first commit

---
+++ perfect_numbers.go
@@ -0,0 +1,41 @@
+package perfect
+
+import "errors"
+
+const testVersion = 1
+
+type Classification int
+
+const (
+	ClassificationPerfect Classification = iota + 1
+	ClassificationAbundant
+	ClassificationDeficient
+)
+
+var ErrOnlyPositive = errors.New("Error")
+
+func Classify(input uint64) (output Classification, err error) {
+	if input < 1 {
+		err = ErrOnlyPositive
+		return
+	}
+	if input == 1 {
+		output = ClassificationDeficient
+		return
+	}
+	sum := uint64(1)
+	for i, t := uint64(2), input; i < t; i++ {
+		if input%i == 0 {
+			sum += i + input/i
+			t = input / i
+		}
+	}
+	if input == sum {
+		output = ClassificationPerfect
+	} else if input > sum {
+		output = ClassificationDeficient
+	} else {
+		output = ClassificationAbundant
+	}
+	return
+}
